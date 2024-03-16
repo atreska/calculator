ce6be587eeb8214f5aa60768b66edff4ba919ec7
atreska <115714570+atreska@users.noreply.github.com>
2024-03-16 11:05:20 -0400

    Create 3 variables & arguments operate function
    This function will take 3 arguments and perform the math operation
    funcitons depending on which operator the user inputs. Example,
    If the user inputs "+" as the operator arguments it will call the add
    function, etc.

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let fistNum = 0;
+let secondNum = 0;
+let operator = '';
+
+function operate(num1, opp, num2) {
+
+    switch (opp) {
+        case '+':
+            console.log(add(num1, num2));
+            break;
+        case '-':
+            console.log(subtract(num1, num2));
+            break;
+        case '*':
+            console.log(multiply(num1, num2));
+            break;
+        case '/':
+            console.log(divide(num1, num2));
+            break;
+        default:
+            console.log('not a correct operator!');
+    }
+
+}
