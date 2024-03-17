214d7a7c3cb94c13a28e16eddcb15e5a35a5c908
atreska <115714570+atreska@users.noreply.github.com>
2024-03-17 09:21:27 -0400

    Take button event listener logic & put into a function

--- script.js
+++ script.js
@@ -35,33 +35,58 @@ function operate(num1, opp, num2) {
 
 }
 
+let num1 = undefined;
+let num2 = undefined;
+let currentTotal = undefined;
+
 let display = document.querySelector('.selection');
 let displayButtons = document.querySelectorAll('.will-display');
 display.textContent = '0'
 
-for (let btn of displayButtons) {
-    btn.addEventListener('click', (e) => {
-        if (display.textContent === '0') {
-            display.textContent = btn.textContent;
-        } else
-            display.textContent += btn.textContent;
+function btnDisplay(buttons) {
+    for (let btn of buttons) {
+        btn.addEventListener('click', (e) => {
+            console.log(currentTotal);
+            if (display.textContent === '0') {
+                display.textContent = btn.textContent;
+            } else if (currentTotal !== undefined) {
+                display.textContent = btn.textContent;
+                console.log('in if statement for buttons')
+            } else
+                display.textContent += btn.textContent;
 
-    })
+        })
+    }
 }
 
-let num1 = 0;
-let num2 = 0;
-
+btnDisplay(displayButtons);
 
 let operators = document.querySelectorAll('.operator');
 
 for (let number1 of operators) {
-    number1.addEventListener('click', () => {
-        num1 = parseInt(display.textContent);
-        console.log(`Num 1 = ${num1}`);
-        operator = number1.textContent;
-        console.log(operator);
-        display.textContent = '';
+    number1.addEventListener('click', (e) => {
+        if (num1 === undefined) {
+            num1 = parseInt(display.textContent);
+            console.log(`Num 1 = ${num1}`);
+            operator = number1.textContent;
+            console.log(operator);
+            display.textContent = '';
+        } else if (num2 === undefined) {
+            num2 = parseInt(display.textContent)
+            console.log(`Num 2 = ${num2}`);
+            display.textContent = '';
+            display.textContent = operate(num1, operator, num2);
+            console.log('in 2nd if!')
+        } else if (num1 !== undefined && num2 !== undefined) {
+            currentTotal = display.textContent;
+            // console.log(currentTotal);
+            // display.textContent = '';
+            display.textContent = operate(currentTotal, operator, num2);
+            console.log('in 3rd if!')
+        } else if (currentTotal !== undefined) {
+            console.log('Were here!')
+        }
+
     })
 }
 
@@ -78,7 +103,7 @@ equals.addEventListener('click', () => {
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
     display.textContent = '0';
-    num1 = 0;
-    num2 = 0;
+    num1 = undefined;
+    num2 = undefined;
 })
 
