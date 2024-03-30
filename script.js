38b9ddffdcc4ec9e6057471eb3d33d9f9145f186
atreska <115714570+atreska@users.noreply.github.com>
2024-03-29 22:15:19 -0400

    Create default 0 & check for division by 0
    Created a function to have a defalt zero show in the display, and also
    checked to see when a number button is clicked, if the display equals 0,
    and if the length is greater than or equal to 1. If so clear the display.
    
    Create conditional statements to check if division by 0 is being
    executed on the operator, and equals listeners. If so, display an error
    message.

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ function clearEverything() {
     currentValue = undefined;
 }
 
+function defaultZero() {
+    display.textContent = '0';
+}
+
+function clearDefaultZero(arr) {
+    if (operationValues[0] == 0 && operationValues[1] == 0 && operationValues[2] == 0) {
+        return true;
+    }
+
+    return false;
+}
+
 function deleteNumber() {
     if (display.textContent == currentValue) {
         return
@@ -65,7 +77,6 @@ function checkArray(arr) {
     return false;
 }
 
-
 function operate(num1, opp, num2) {
 
     switch (opp) {
@@ -86,12 +97,28 @@ function operate(num1, opp, num2) {
 let operationValues = ['', '', ''];
 
 
-
 let display = document.querySelector(".selection");
+defaultZero();
+
+//on and off switch to clear the current total the first time around.
+let clearTotal = 0;
 
 let numButtons = document.querySelectorAll('.num');
 for (let btn of numButtons) {
     btn.addEventListener('click', (e) => {
+        // if (display.textContent.length >= 9) {
+        //     return;
+        // }
+
+        if (display.textContent == 0 && display.textContent.length <= 1) {
+            display.textContent = '';
+        }
+        //used to delete the current value that is showing 
+        if (clearTotal === 1) {
+            display.textContent = '';
+            clearTotal = 0;
+        }
+
         clearDisplay();
         appendNumber(e.target.textContent);
     })
@@ -100,12 +127,14 @@ for (let btn of numButtons) {
 let decimal = document.querySelector('.decimal');
 decimal.addEventListener('click', (e) => {
     appendDecimal(decimal);
+    console.log(display.textContent);
+
 })
 
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', (e) => {
     clearEverything();
-
+    display.textContent = 0;
 });
 
 let deleteANumber = document.querySelector('.delete');
@@ -119,15 +148,23 @@ let operator = document.querySelectorAll('.operator');
 
 for (let btn of operator) {
     btn.addEventListener('click', (e) => {
+
         if (checkArray(operationValues)) {
             console.log(operationValues);
             if (operationValues[0] === '') {
                 operationValues[0] = parseFloat(display.textContent);
-                display.textContent = '';
+                // display.textContent = '';
+                clearTotal = 1;
             } else if (operationValues[1] === '') {
                 operationValues[1] = e.target.textContent;
             } else {
                 operationValues[2] = parseFloat(display.textContent);
+
+                if (operationValues[1] == '/' && operationValues[2] == 0) {
+                    display.textContent = 'Error';
+                    return;
+                }
+
                 display.textContent = operate(operationValues[0], operationValues[1], operationValues[2]);
                 currentValue = operate(operationValues[0], operationValues[1], operationValues[2]);
                 clearArray(operationValues);
@@ -144,6 +181,7 @@ for (let btn of operator) {
 
 let equals = document.querySelector('.equals');
 equals.addEventListener('click', (e) => {
+
     if (checkArray(operationValues)) {
         if (operationValues[0] === '') {
             return
@@ -151,6 +189,13 @@ equals.addEventListener('click', (e) => {
             return;
         } else if (operationValues[2] === '') {
             operationValues[2] = parseFloat(display.textContent);
+
+            //division by zero error
+            if (operationValues[1] == '/' && operationValues[2] == 0) {
+                display.textContent = 'Error';
+                return;
+            }
+
             display.textContent = operate(operationValues[0], operationValues[1], operationValues[2]);
             currentValue = operate(operationValues[0], operationValues[1], operationValues[2]);
             clearArray(operationValues);
